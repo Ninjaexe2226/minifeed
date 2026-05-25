620892b3377d7aebda4781c92e6399c6e6daaced
Ninjaexe2226 <105080803+Ninjaexe2226@users.noreply.github.com>
2026-05-24 21:00:48 -0500

    Add files via upload

---
+++ js/utils.js
@@ -0,0 +1,21 @@
+function getRelativeTime(timestampMs) {
+  const now = Date.now();
+  const diff = Math.floor((now - timestampMs) / 1000);
+  if (diff < 60) return "just now";
+  if (diff < 3600) return `${Math.floor(diff / 60)}m ago`;
+  if (diff < 86400) return `${Math.floor(diff / 3600)}h ago`;
+  return `${Math.floor(diff / 86400)}d ago`;
+}
+
+function escapeHtml(str) {
+  return String(str)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
+
+function nl2brSafe(str) {
+  return escapeHtml(str).replace(/\n/g, "<br>");
+}
